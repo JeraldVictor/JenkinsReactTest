879364fdefec99e1355430b8877149b2983e4721
Jerald Victor <Jeraldvictor.Bj@cerner.com>
2023-06-09 09:50:36 +0530

    added sub method

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,9 @@
-import sum from '../functions/add'
+import { add } from "../functions/add"
 
 test('adds 1 + 2 to equal 3', () => {
-  console.log("checked")
-  // expect(sum(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(3);
+});
+
+test('adds 1 + string to equal NAN', () => {
+  expect(add(1, 'aaaa')).toBe('1aaaa');
 });
